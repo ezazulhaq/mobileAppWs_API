ec5c1a9b05a1074480f153c4ddf1f057144eedbb
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-03-29 04:49:46 +0530

    Return Custome Error Object Representation and all other exceptions

--- src/main/java/com/haa/app/ws/exception/AppExceptionsHandler.java
+++ src/main/java/com/haa/app/ws/exception/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.haa.app.ws.exception;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +9,25 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.haa.app.ws.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 	
 	@ExceptionHandler(value = { UserServiceException.class })
 	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest req)
 	{
-		return new ResponseEntity<Object>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<Object>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value = { Exception.class })
+	public ResponseEntity<Object> handleUserServiceException(Exception ex, WebRequest req)
+	{
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<Object>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
