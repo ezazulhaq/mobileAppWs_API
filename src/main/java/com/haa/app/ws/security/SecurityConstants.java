cf0ae6577a8efb732508721f127010d6cf61de9f
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-03-28 20:08:52 +0530

    Reading Token Secret from a property file

--- src/main/java/com/haa/app/ws/security/SecurityConstants.java
+++ src/main/java/com/haa/app/ws/security/SecurityConstants.java
@@ -1,11 +1,20 @@
 package com.haa.app.ws.security;
 
+import com.haa.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 	
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGNUP_URL = "/users";
-	public static final String TOKEN_SECRET = "asfaskjfgbs76";
+	//public static final String TOKEN_SECRET = "asfaskjfgbs76";		// Token secret read from application.properties file
+	
+	public static String getTokenSecret() {
+		
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+		return appProperties.getTokenSceret();
+		
+	}
 
 }
