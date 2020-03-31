5d3e7b5b257cbd2673e153564dd12ea612c16629
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-04-01 00:57:01 +0530

    Get Users Request URL with parameters by creating a user resource method Also Create get User Service Layer Method for pagination and Limit

--- src/main/java/com/haa/app/ws/io/repositories/UserRepository.java
+++ src/main/java/com/haa/app/ws/io/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package com.haa.app.ws.io.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.haa.app.ws.io.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 	
 	UserEntity findUserByEmail(String email);
 	UserEntity findByUserId(String userId);
--- src/main/java/com/haa/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/haa/app/ws/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.haa.app.ws.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -123,6 +126,28 @@ public class UserServiceImpl implements UserService {
 		
 		userRepository.delete(userEntity);
 		
+	}
+
+	@Override
+	public List<UserDto> getUsers(int page, int limit) {
+		
+		List<UserDto> returnValues = new ArrayList<UserDto>();
+		
+		if(page > 0)
+			page = page - 1;
+		
+		PageRequest pagableRequest = PageRequest.of(page, limit);
+		
+		Page<UserEntity> usersPage = userRepository.findAll(pagableRequest);
+		List<UserEntity> users = usersPage.getContent();
+		
+		for (UserEntity userEntity : users) {
+			UserDto userDto = new UserDto();
+			BeanUtils.copyProperties(userEntity, userDto);
+			returnValues.add(userDto);
+		}
+		
+		return returnValues;
 	}
 
 }
