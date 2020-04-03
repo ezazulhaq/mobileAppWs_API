b32e69ab258a7f74f66149ccb6d95267cfc43e6a
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-04-03 19:35:49 +0530

    Create AddressEntity and map ManyToOne relationship to UserDto

--- src/main/java/com/haa/app/ws/io/entity/AddressEntity.java
+++ src/main/java/com/haa/app/ws/io/entity/AddressEntity.java
@@ -1,14 +1,46 @@
-package com.haa.app.ws.shared.dto;
+package com.haa.app.ws.io.entity;
 
-public class AddressDto {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.haa.app.ws.shared.dto.UserDto;
+
+@Entity(name="addresses")
+public class AddressEntity implements Serializable {
+
+	private static final long serialVersionUID = 2027652153196976411L;
+	
+	@Id
+	@GeneratedValue
 	private long id;
+	
+	@Column(length = 30, nullable = false)
 	private String addressId;
+	
+	@Column(length = 15, nullable = false)
 	private String city;
+
+	@Column(length = 15, nullable = false)
 	private String country;
+
+	@Column(length = 100, nullable = false)
 	private String streetName;
+
+	@Column(length = 7, nullable = false)
 	private String postalCode;
+
+	@Column(length = 10, nullable = false)
 	private String type;
+
+	@ManyToOne
+	@JoinColumn(name = "users_id")
+	private UserDto userDetails;
 
 	public long getId() {
 		return id;
@@ -64,6 +96,14 @@ public class AddressDto {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public UserDto getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserDto userDetails) {
+		this.userDetails = userDetails;
 	}
 
 }
